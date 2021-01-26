1937fb5c5ac79f61dfe80c0e1fa0e93b9c84bbe9
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2021-01-26 20:03:21 +0100

    Add Schedule and ScheduleItem autoMapper profiles

---
+++ ScheduleGenerator/Server/Shared/Profiles/ScheduleItemProfile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using ScheduleGenerator.Server.Models;
+using ScheduleGenerator.Shared.Dto;
+
+namespace ScheduleGenerator.Server.Shared.Profiles
+{
+    public class ScheduleItemProfile : Profile
+    {
+        public ScheduleItemProfile()
+        {
+            CreateMap<ScheduleItem, ScheduleItemDto>();
+            CreateMap<ScheduleItemForCreationDto, ScheduleItem>();
+            CreateMap<ScheduleItemForUpdateDto, ScheduleItem>();
+            CreateMap<ScheduleItem, ScheduleItemForUpdateDto>();
+        }
+    }
+}
---
+++ ScheduleGenerator/Server/Shared/Profiles/ScheduleProfile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using ScheduleGenerator.Server.Models;
+using ScheduleGenerator.Shared.Dto;
+
+namespace ScheduleGenerator.Server.Shared.Profiles
+{
+    public class ScheduleProfile : Profile
+    {
+        public ScheduleProfile()
+        {
+            CreateMap<Schedule, ScheduleDto>();
+            CreateMap<ScheduleForCreationDto, Schedule>();
+            CreateMap<ScheduleForUpdateDto, Schedule>();
+            CreateMap<Schedule, ScheduleForUpdateDto>();
+        }
+    }
+}
