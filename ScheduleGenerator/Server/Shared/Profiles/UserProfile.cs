165f7cc240af223aaff6d800d0e1325937f169e3
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2021-01-26 11:25:55 +0100

    Add AutoMapper profile for Users

---
+++ ScheduleGenerator/Server/Shared/Profiles/UserProfile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using ScheduleGenerator.Server.Models;
+using ScheduleGenerator.Shared.Dto;
+
+namespace ScheduleGenerator.Server.Shared.Profiles
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<User, UserDto>();
+            CreateMap<UserForCreationDto, User>();
+        }
+    }
+}
