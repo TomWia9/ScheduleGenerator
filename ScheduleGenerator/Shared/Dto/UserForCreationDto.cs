f33f263044d6e83cce47d2217d17ce1bed42f057
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2021-01-26 11:19:11 +0100

    Change range attribute to min and max length

--- ScheduleGenerator/Shared/Dto/UserForCreationDto.cs
+++ ScheduleGenerator/Shared/Dto/UserForCreationDto.cs
@@ -11,17 +11,20 @@ namespace ScheduleGenerator.Shared.Dto
     public class UserForCreationDto
     {
         [Required(AllowEmptyStrings = false)]
-        [Range(3, 40, ErrorMessage = "The Email field must be between {1} and {2}.")]
+        [MaxLength(20, ErrorMessage = "The Email field may contain at most 20 characters.")]
+        [MinLength(3, ErrorMessage = "The Email field must contain at least 3 characters.")]
         [EmailAddress]
         public string Email { get; set; }
 
         [Required(AllowEmptyStrings = false)]
-        [Range(5, 20, ErrorMessage = "The Password field must be between {1} and {2}.")]
+        [MaxLength(20, ErrorMessage = "The Password field may contain at most 20 characters.")]
+        [MinLength(5, ErrorMessage = "The Password field must contain at least 5 characters.")]
         public string Password { get; set; }
         
         [DisplayName("Confirm password")]
         [Required(AllowEmptyStrings = false, ErrorMessage = "The Confirm Password field is required.")]
-        [Range(5, 20, ErrorMessage = "The Confirm Password field must be between {1} and {2}.")]
+        [MaxLength(20, ErrorMessage = "The Confirm Password field may contain at most 20 characters.")]
+        [MinLength(5, ErrorMessage = "The Confirm Password field must contain at least 5 characters.")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
     }
