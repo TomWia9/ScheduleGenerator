629d580ec38b3943801561057b2443482c830206
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2021-02-01 23:10:01 +0100

    Add SchedulesService

---
+++ ScheduleGenerator/Client/Services/ISchedulesService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using ScheduleGenerator.Shared.Dto;
+
+namespace ScheduleGenerator.Client.Services
+{
+    public interface ISchedulesService
+    {
+        Task<HttpResponseMessage> CreateSchedule(int scheduleId, ScheduleForCreationDto schedule);
+        Task<HttpResponseMessage> UpdateSchedule(int scheduleId, ScheduleForUpdateDto schedule);
+        Task<HttpResponseMessage> DeleteSchedule(int scheduleId);
+    }
+}
---
+++ ScheduleGenerator/Client/Services/SchedulesService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using ScheduleGenerator.Shared.Dto;
+
+namespace ScheduleGenerator.Client.Services
+{
+    public class SchedulesService : ISchedulesService
+    {
+        private readonly IHttpService _httpService;
+
+        public SchedulesService(IHttpService httpService)
+        {
+            _httpService = httpService;
+        }
+
+        public async Task<HttpResponseMessage> CreateSchedule(int scheduleId, ScheduleForCreationDto schedule)
+        {
+            return await _httpService.Post($"api/schedules", schedule);
+
+        }
+
+        public async Task<HttpResponseMessage> UpdateSchedule(int scheduleId, ScheduleForUpdateDto schedule)
+        {
+            return await _httpService.Put($"api/schedules/{scheduleId}", schedule);
+        }
+
+        public async Task<HttpResponseMessage> DeleteSchedule(int scheduleId)
+        {
+            return await _httpService.Delete($"api/schedules/{scheduleId}");
+        }
+    }
+}
