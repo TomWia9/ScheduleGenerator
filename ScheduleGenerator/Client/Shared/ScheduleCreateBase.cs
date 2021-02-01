dcac09a4a0198d2be8dde0a2f50424ed537a446a
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2021-02-01 21:43:22 +0100

    Add ScheduleItem, ui changes in ScheduleCreate

--- ScheduleGenerator/Client/Shared/ScheduleCreateBase.cs
+++ ScheduleGenerator/Client/Shared/ScheduleCreateBase.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
 using ScheduleGenerator.Client.Shared.Modals;
 using ScheduleGenerator.Shared.Dto;
+using ScheduleGenerator.Shared.Enums;
 
 namespace ScheduleGenerator.Client.Shared
 {
@@ -13,6 +14,104 @@ namespace ScheduleGenerator.Client.Shared
         protected ScheduleForCreationDto ScheduleForCreation = new();
         protected CreateScheduleItemModal CreateScheduleItemModal;
 
+        //mock data for now
+        protected List<ScheduleItemDto> Items = new List<ScheduleItemDto>
+        {
+            new ScheduleItemDto()
+            {
+                Id = 1,
+                Subject = "Technika mikroprocesorowa",
+                RoomNumber = "E9",
+                Lecturer = "Mr. Test",
+                DayOfWeek = DayOfWeek.Tuesday,
+                StartTime = TimeSpan.Parse("07:00"),
+                EndTime = TimeSpan.Parse("08:00"),
+                TypeOfClasses = TypeOfClasses.Exercises,
+                Color = Color.Success,
+                ScheduleId = 1
+    },
+
+            new ScheduleItemDto()
+            {
+                Id = 2,
+                Subject = "Aplikacje internetowe",
+                RoomNumber = "E12",
+                Lecturer = "Mr. Test",
+                DayOfWeek = DayOfWeek.Wednesday,
+                StartTime = TimeSpan.Parse("09:00"),
+                EndTime = TimeSpan.Parse("10:45"),
+                TypeOfClasses = TypeOfClasses.Lecture,
+                Color = Color.Danger,
+                ScheduleId = 1
+            },
+
+            new ScheduleItemDto()
+            {
+                Id = 3,
+                Subject = "Język C#",
+                RoomNumber = "c9",
+                Lecturer = "Mr. Test",
+                DayOfWeek = DayOfWeek.Friday,
+                StartTime = TimeSpan.Parse("07:00"),
+                EndTime = TimeSpan.Parse("09:00"),
+                TypeOfClasses = TypeOfClasses.Project,
+                Color = Color.Secondary,
+                ScheduleId = 1
+            },
+            new ScheduleItemDto()
+            {
+                Id = 3,
+                Subject = "Język C#",
+                RoomNumber = "c9",
+                Lecturer = "Mr. Test",
+                DayOfWeek = DayOfWeek.Friday,
+                StartTime = TimeSpan.Parse("07:00"),
+                EndTime = TimeSpan.Parse("09:00"),
+                TypeOfClasses = TypeOfClasses.Project,
+                Color = Color.Secondary,
+                ScheduleId = 1
+            },
+            new ScheduleItemDto()
+            {
+                Id = 3,
+                Subject = "Język C#",
+                RoomNumber = "c9",
+                Lecturer = "Mr. Test",
+                DayOfWeek = DayOfWeek.Friday,
+                StartTime = TimeSpan.Parse("07:00"),
+                EndTime = TimeSpan.Parse("09:00"),
+                TypeOfClasses = TypeOfClasses.Project,
+                Color = Color.Secondary,
+                ScheduleId = 1
+            },
+            new ScheduleItemDto()
+            {
+                Id = 3,
+                Subject = "Język C#",
+                RoomNumber = "c9",
+                Lecturer = "Mr. Test",
+                DayOfWeek = DayOfWeek.Thursday,
+                StartTime = TimeSpan.Parse("07:00"),
+                EndTime = TimeSpan.Parse("09:00"),
+                TypeOfClasses = TypeOfClasses.Seminar,
+                Color = Color.Warning,
+                ScheduleId = 1
+            },
+            new ScheduleItemDto()
+            {
+                Id = 3,
+                Subject = "Język C#",
+                RoomNumber = "c9",
+                Lecturer = "Mr. Test",
+                DayOfWeek = DayOfWeek.Monday,
+                StartTime = TimeSpan.Parse("07:00"),
+                EndTime = TimeSpan.Parse("09:00"),
+                TypeOfClasses = TypeOfClasses.Project,
+                Color = Color.Secondary,
+                ScheduleId = 1
+            },
+        };
+        
         protected bool Loading;
 
 
