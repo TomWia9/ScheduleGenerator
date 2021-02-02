a424e6095dc871a2c00924e634a40e3ace650f0a
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2021-02-02 20:31:35 +0100

    Add NewSchedule page

---
+++ ScheduleGenerator/Client/Pages/NewScheduleBase.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using ScheduleGenerator.Client.Services;
+using ScheduleGenerator.Shared.Dto;
+
+namespace ScheduleGenerator.Client.Pages
+{
+    public class NewScheduleBase : ComponentBase
+    {
+        [Inject]
+        private IAuthenticationService AuthenticationService { get; set; }
+        [Inject]
+        private ISchedulesService SchedulesService { get; set; }
+
+        [Inject]
+        private NavigationManager NavigationManager { get; set; }
+        protected ScheduleForCreationDto ScheduleForCreation { get; set; } = new();
+        protected bool Conflict { get; set; }
+        protected bool CreationFailed { get; set; }
+
+        protected override void OnInitialized()
+        {
+            //TODO Implement anonymous page where anonymous user can create schedule without saving it
+            //if (!AuthenticationService.IsUserLoggedIn())
+            //{
+            //    NavigationManager.NavigateTo("schedules/anonymous");
+            //}
+        }
+
+        protected async Task CreateScheduleAsync()
+        {
+            var response = await SchedulesService.CreateScheduleAsync(ScheduleForCreation);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var schedule = await response.Content.ReadFromJsonAsync<ScheduleDto>();
+                
+                //TODO Add schedule to AppState
+                NavigationManager.NavigateTo($"schedules/{schedule.Id}");
+            }
+            else if (response.StatusCode == HttpStatusCode.Conflict)
+            {
+                Conflict = true;
+            }
+            else
+            {
+                CreationFailed = true;
+            }
+        }
+    }
+}
