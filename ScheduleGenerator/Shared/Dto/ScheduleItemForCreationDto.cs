023bbe52ca8e1616b91359907d367329900180e2
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2021-02-02 15:32:15 +0100

    Set default day of week to monday for ScheduleItem

--- ScheduleGenerator/Shared/Dto/ScheduleItemForCreationDto.cs
+++ ScheduleGenerator/Shared/Dto/ScheduleItemForCreationDto.cs
@@ -12,6 +12,7 @@ namespace ScheduleGenerator.Shared.Dto
         {
             StartTime = DateTime.Parse("2015-05-16T07:00:00");
             EndTime = DateTime.Parse("2015-05-16T08:00:00");
+            DayOfWeek = DayOfWeek.Monday;
         }
     }
 }
