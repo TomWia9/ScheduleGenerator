94694313c0b15cfe85b9265438e8e0b8c40e652c
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2021-02-08 08:57:10 +0100

    Add custom matchPassword validator with password regules

---
+++ ScheduleGenerator/Shared/Validators/CustomValidators.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+
+namespace ScheduleGenerator.Shared.Validators
+{
+    public static class CustomValidators
+    {
+        public static IRuleBuilderInitial<T, string> MatchPassword<T>(this IRuleBuilder<T, string> ruleBuilder)
+        {
+            return ruleBuilder.Custom((password, context) =>
+            {
+                if (!password.Any(char.IsDigit))
+                {
+                    context.AddFailure("Password must contain at least one number");
+                }
+
+                if (!password.Any(char.IsLetter))
+                {
+                    context.AddFailure("Password must contain at least one letter");
+                }
+
+                if (!password.Any(char.IsUpper))
+                {
+                    context.AddFailure("Password must contain at least one upper letter");
+                }
+
+            });
+        }
+    }
+}
