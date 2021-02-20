8efa8cd7ecf3aa537966885ce91e9a40bb3e9988
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2021-02-20 11:40:30 +0100

    Add CascadeMode.Stop to Password Rule in UserForCreationValidator

--- ScheduleGenerator/Shared/Validators/UserForCreationValidator.cs
+++ ScheduleGenerator/Shared/Validators/UserForCreationValidator.cs
@@ -9,7 +9,7 @@ namespace ScheduleGenerator.Shared.Validators
         {
             RuleFor(u => u.Email).NotEmpty().Length(3, 20).EmailAddress();
 
-            RuleFor(u => u.Password).NotEmpty().Length(5, 20).MatchPassword();
+            RuleFor(u => u.Password).Cascade(CascadeMode.Stop).NotEmpty().Length(5, 20).MatchPassword();
 
             RuleFor(u => u.ConfirmPassword).NotEmpty().Length(5, 20).Equal(u => u.Password)
                 .When(u => !string.IsNullOrWhiteSpace(u.Password)).WithMessage("Passwords do not match");
